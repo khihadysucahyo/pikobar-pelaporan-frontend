981dc5bab28b1e0889b5bd005e8bf7ebecca60e3
Arif <arifboyz16@gmail.com>
2020-04-23 16:59:56 +0700

    vue google analytics

--- src/main.js
+++ src/main.js
@@ -25,7 +25,6 @@ import '@/styles' // include all css files
 import './permission' // permission control
 import '@/utils/vee-validate' // include all validate form
 import i18n from './lang' // Internationalization
-import '@/helpers/filters' // include all filters
 // import './registerServiceWorker'
 
 Vue.use(FlagIcon)
