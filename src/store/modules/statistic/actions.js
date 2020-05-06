1617e4686e9abfeeda794d669a3bd6f239f351d1
Rendi <rendirizkir@gmail.com>
2020-05-06 14:11:06 +0700

    fix: data chart confirmed

--- src/store/modules/statistic/actions.js
+++ src/store/modules/statistic/actions.js
@@ -44,38 +44,11 @@ export default {
   async countPositive({
     commit
   }, params) {
-    // try {
-    //   const response = await requestServer('/api/dashboard/chart-pdp', 'GET', params)
-    //   return response
-    // } catch (error) {
-    //   return error.response
-    // }
-
-    const result = {
-      'status': 200,
-      'message': 'Success',
-      'data': []
+    try {
+      const response = await requestServer('/api/dashboard/chart-confirm', 'GET', params)
+      return response
+    } catch (error) {
+      return error.response
     }
-    result.data.push({
-      'sembuh': 1,
-      'meninggal': 2,
-      'positif': 3,
-      'total': 249,
-      'date': '2020/03/23',
-      'cum_sembuh': 1,
-      'cum_meninggal': 2,
-      'cum_positif': 3
-    })
-    result.data.push({
-      'sembuh': 1,
-      'meninggal': 2,
-      'positif': 3,
-      'total': 249,
-      'date': '2020/03/24',
-      'cum_sembuh': 2,
-      'cum_meninggal': 4,
-      'cum_positif': 6
-    })
-    return result
   }
 }
