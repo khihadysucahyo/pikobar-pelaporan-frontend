c16fa8ab54e875a1fb72daf578c271fbedcbe17e
naufalihsank <naufalihsank@gmail.com>
2020-05-10 21:54:59 +0700

    Perbaikan iterasi checkbox

--- src/components/Pelaporan/FormInformationHistory.vue
+++ src/components/Pelaporan/FormInformationHistory.vue
@@ -286,8 +286,8 @@
             />
             <ValidationProvider v-slot="{ errors }">
               <label>{{ $t('label.symptoms') }}</label>
-              <v-row v-for="rowIdx in Math.ceil(symptomOptions.length / 3)" :key="rowIdx">
-                <v-col v-for="item in symptomOptions.slice(3 * (rowIdx - 1), 3 * rowIdx)" :key="item" class="one-third column">
+              <v-row>
+                <v-col v-for="item in symptomOptions" :key="item" sm="4" md="4">
                   <label class="material-checkbox-custom">
                     <input
                       v-model="formPasien.diagnosis"
