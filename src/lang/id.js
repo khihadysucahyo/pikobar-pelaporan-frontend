32348cfc4abd91df8fcef0ca54e41242bd1a3f62
naufalihsank <naufalihsank@gmail.com>
2020-05-29 14:59:26 +0700

    Revert "Print PE Form"
    This reverts commit 9f11365a787dd6675c9af53fb32df8125426c232.

--- src/lang/id.js
+++ src/lang/id.js
@@ -410,8 +410,7 @@ export default {
     disclaimer: 'Disclaimer',
     map_disclaimer_step_1: 'Pembaruan jumlah titik sebaran membutuhkan waktu hingga 1 jam setelah data diinput ke sistem melalui fitur “Laporan Kasus”.',
     map_disclaimer_step_2: 'Penentuan titik lokasi hanya berdasarkan lokasi Kelurahan dan tidak menunjuk pada alamat persis masing-masing kasus.',
-    understand: 'Mengerti',
-    print_pe_form: 'Cetak Laporan PE'
+    understand: 'Mengerti'
   },
   success: {
     login_success: 'Login Berhasil',
