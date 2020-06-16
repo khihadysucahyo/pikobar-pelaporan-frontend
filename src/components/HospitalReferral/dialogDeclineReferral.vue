86c44fbb606e834ee0fed5ed5e6e140528183ef3
Arif <arifboyz16@gmail.com>
2020-06-16 15:04:49 +0700

    dialog decline referral

--- src/components/HospitalReferral/dialogDeclineReferral.vue
+++ src/components/HospitalReferral/dialogDeclineReferral.vue
@@ -3,95 +3,100 @@
     <v-dialog
       v-model="dialogPopup"
       persistent
-      max-width="40%"
+      max-width="30%"
     >
-      <v-card>
-        <v-card-title class="justify-center">
-          <img src="@/static/hospital_reference_1.svg">
-        </v-card-title>
-        <v-card-title class="justify-center">
-          <div class="font-weight-bold" style="font-size: 16px">
-            Verifikasi Penolakan Pasien
-          </div>
-        </v-card-title>
-        <v-container>
-          <ValidationObserver ref="observer">
-            <v-form
-              ref="form"
-              lazy-validation
-            >
+      <ValidationObserver ref="observer">
+        <v-form
+          ref="form"
+          lazy-validation
+        >
+          <v-card>
+            <v-card-title class="justify-center">
+              <img src="@/static/hospital_reference_1.svg">
+            </v-card-title>
+            <v-card-title class="justify-center">
+              <div class="font-weight-bold" style="font-size: 16px">
+                Verifikasi Penolakan Pasien
+              </div>
+            </v-card-title>
+            <v-container>
               <v-row>
                 <v-col>
-                  <label>{{ $t('label.search_patient_by_name_nik') }}</label>
+                  <label class="require">Berikan Alasan Penolakan</label>
                   <ValidationProvider
                     v-slot="{ errors }"
+                    rules="required"
                   >
                     <v-textarea
-                      v-model="formReferral.address_street"
-                      :error="errors"
+                      v-model="formReferral.transfer_comment"
+                      placeholder="Rujukan ditolak karena ..."
+                      :error-messages="errors"
                       solo
                     />
                   </ValidationProvider>
                 </v-col>
               </v-row>
-            </v-form>
-          </ValidationObserver>
-        </v-container>
-        <v-card-actions>
-          <v-col class="text-right">
-            <v-btn
-              class="mr-5"
-              style="height: 40px;min-width: 120px;"
-              @click="closeData"
-            >
-              {{ $t('label.canceled') }}
-            </v-btn>
-            <v-btn
-              color="primary"
-              style="height: 40px;min-width: 120px;"
-              @click="onHandleSave"
-            >
-              {{ $t('label.decline').toUpperCase() }}
-            </v-btn>
-          </v-col>
-        </v-card-actions>
-      </v-card>
+            </v-container>
+            <v-card-actions>
+              <v-col>
+                <v-btn
+                  class="mr-5"
+                  block
+                  @click="closeData"
+                >
+                  {{ $t('label.canceled') }}
+                </v-btn>
+              </v-col>
+              <v-col>
+                <v-btn
+                  color="#EB5757"
+                  block
+                  @click="onHandleSave"
+                >
+                  {{ $t('label.decline').toUpperCase() }}
+                </v-btn>
+              </v-col>
+            </v-card-actions>
+          </v-card>
+        </v-form>
+      </ValidationObserver>
     </v-dialog>
   </v-row>
 </template>
 
 <script>
-import { ValidationObserver } from 'vee-validate'
+import { ValidationObserver, ValidationProvider } from 'vee-validate'
 import { mapGetters } from 'vuex'
-import { ResponseRequest } from '@/utils/constantVariable'
 export default {
   name: 'DeclineReferral',
   components: {
-    ValidationObserver
+    ValidationObserver,
+    ValidationProvider
   },
   props: {
-    dialog: {
+    dialogDecline: {
       type: Boolean,
       default: false
     },
-    patientRegistered: {
-      type: Boolean,
-      default: false
+    detailTransfer: {
+      type: Object,
+      default: null
     },
-    formReferral: {
+    detailCase: {
       type: Object,
       default: null
     }
   },
   data() {
     return {
-      dialogReferral: false
+      dialogReferral: false,
+      formReferral: {}
     }
   },
   computed: {
     dialogPopup: {
       get() {
-        return this.dialog
+        return this.dialogDecline
       },
       set(val) {
         this.$emit('update:dialogPopup', val)
@@ -103,28 +108,31 @@ export default {
   },
   methods: {
     async closeData() {
-      await this.$emit('update:dialogPopup', false)
-      await this.$emit('update:referralForm', {})
+      await this.$emit('update:showDecline', false)
+      // this.$emit('update:caseDetail', {})
+      // this.$emit('update:transferDetail', {})
+      this.formReferral = {}
     },
     async onHandleSave() {
-      if (this.patientRegistered) {
-        const data = {
-          id: this.formReferral.case_id,
-          data: {
-            transfer_to_unit_id: this.formReferral.transfer_to_unit._id,
-            transfer_to_unit_name: this.formReferral.transfer_to_unit.name
-          }
-        }
-        const response = await this.$store.dispatch('reports/caseHospitalRefferal', data)
-        if (response.status !== ResponseRequest.UNPROCESSABLE) {
-          await this.$emit('update:dialogPopup', false)
-          await this.$emit('update:referralForm', {})
-        } else {
-          await this.$emit('update:dialogPopup', false)
-          await this.$emit('update:referralForm', {})
-          this.dialogReferral = true
-        }
+      const valid = await this.$refs.observer.validate()
+      console.log(valid)
+      if (!valid) {
+        return
       }
+      const data = {
+        idCase: this.detailCase._id,
+        idTransfer: this.detailTransfer._id,
+        actions: 'decline',
+        body: this.formReferral
+      }
+      const response = await this.$store.dispatch('reports/actionHospitalReferral', data)
+      if (response) {
+        this.$emit('update:caseDetail', {})
+        this.$emit('update:transferDetail', {})
+        this.$emit('update:showDecline', false)
+        await this.$store.dispatch('toast/successToast', 'Rujukan Ditolak')
+      }
+      this.formReferral = {}
     }
   }
 }
