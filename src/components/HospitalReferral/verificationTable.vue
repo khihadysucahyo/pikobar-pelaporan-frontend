478f78c0862691b7f3a3e07fdd5acf06494a3d38
Arif <arifboyz16@gmail.com>
2020-06-26 15:22:57 +0700

    request sintha

--- src/components/HospitalReferral/verificationTable.vue
+++ src/components/HospitalReferral/verificationTable.vue
@@ -25,7 +25,14 @@
               {{ $t('label.done') }}
             </div>
           </td>
-          <td> {{ item.transfer_to_unit_name }} </td>
+          <td>
+            <div v-if="typeReferral === 'out' || unitType === 'puskemas'">
+              {{ item.transfer_to_unit_name }}
+            </div>
+            <div v-else>
+              {{ item.transfer_from_unit_name }}
+            </div>
+          </td>
           <td><status-referral :status="item.transfer_status" /></td>
           <td>
             <v-card-actions
@@ -117,6 +124,10 @@ export default {
     listReferral: {
       type: Array,
       default: null
+    },
+    typeReferral: {
+      type: String,
+      default: ''
     },
     listQuery: {
       type: Object,
